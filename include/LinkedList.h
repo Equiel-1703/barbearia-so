5253b37e06b91c936acee401faa81fdc32b46b7d
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-02-26 21:03:33 -0300

    Fixed bug in LinkedList

--- include/LinkedList.h
+++ include/LinkedList.h
@@ -4,44 +4,55 @@
 #include <pthread.h>
 
 template <typename T>
-class LinkedList {
+class LinkedList
+{
 private:
-    struct Node {
+    struct Node
+    {
         T data;
-        Node* next;
+        Node *next;
     };
 
-    Node* head;
-    int size = 0;
+    Node *head;
+    int size;
     // linked list mutex
     pthread_mutex_t mutex;
 
 public:
-    LinkedList() : head(nullptr) {
+    LinkedList() : head(nullptr)
+    {
+        size = 0;
         pthread_mutex_init(&mutex, NULL);
     }
 
-    ~LinkedList() {
-        while (head != nullptr) {
-            Node* temp = head;
+    ~LinkedList()
+    {
+        while (head != nullptr)
+        {
+            Node *temp = head;
             head = head->next;
             delete temp;
         }
         pthread_mutex_destroy(&mutex);
     }
 
-    void push_back(const T& value) {
+    void push_back(const T &value)
+    {
         pthread_mutex_lock(&mutex);
         size++;
-        Node* newNode = new Node;
+        Node *newNode = new Node;
         newNode->data = value;
         newNode->next = nullptr;
 
-        if (head == nullptr) {
+        if (head == nullptr)
+        {
             head = newNode;
-        } else {
-            Node* current = head;
-            while (current->next != nullptr) {
+        }
+        else
+        {
+            Node *current = head;
+            while (current->next != nullptr)
+            {
                 current = current->next;
             }
             current->next = newNode;
@@ -49,46 +60,78 @@ public:
         pthread_mutex_unlock(&mutex);
     }
 
-    // Pop
-    T pop() {
+    // Pop de debug
+    T pop(const char *label)
+    {
         pthread_mutex_lock(&mutex);
-        size--;
-        if (head == nullptr) {
+        if (head == nullptr)
+        {
+            std::cerr << label << std::endl;
             std::cerr << "List is empty" << std::endl;
-            return T();
+            pthread_mutex_unlock(&mutex);
+            return NULL;
         }
 
-        Node* temp = head;
+        Node *temp = head;
         head = head->next;
         T value = temp->data;
         delete temp;
+        size--;
         pthread_mutex_unlock(&mutex);
         return value;
     }
 
-    int getSize() {
+    // Pop
+    T pop()
+    {
+        pthread_mutex_lock(&mutex);
+        if (head == nullptr)
+        {
+            // std::cerr << "List is empty" << std::endl;
+            pthread_mutex_unlock(&mutex);
+            return NULL;
+        }
+
+        Node *temp = head;
+        head = head->next;
+        T value = temp->data;
+        delete temp;
+        size--;
+        pthread_mutex_unlock(&mutex);
+        return value;
+    }
+
+    int getSize()
+    {
         pthread_mutex_lock(&mutex);
         int tmp_size = this->size;
         pthread_mutex_unlock(&mutex);
         return tmp_size;
     }
 
-    bool isEmpty() {
+    bool isEmpty()
+    {
         pthread_mutex_lock(&mutex);
         bool empty = this->size == 0;
         pthread_mutex_unlock(&mutex);
         return empty;
     }
 
-    T remove(const T& value) {
+    T remove(const T &value)
+    {
         pthread_mutex_lock(&mutex);
-        Node* current = head;
-        Node* previous = nullptr;
-        while (current != nullptr) {
-            if (current->data == value) {
-                if (previous == nullptr) {
+        Node *current = head;
+        Node *previous = nullptr;
+        while (current != nullptr)
+        {
+            if (current->data == value)
+            {
+                if (previous == nullptr)
+                {
                     head = current->next;
-                } else {
+                }
+                else
+                {
                     previous->next = current->next;
                 }
                 T data = current->data;
@@ -100,15 +143,17 @@ public:
             previous = current;
             current = current->next;
         }
-        
+
         std::cout << "Value not found" << std::endl;
         pthread_mutex_unlock(&mutex);
         return T();
     }
 
-    const T& front() {
+    const T &front()
+    {
         pthread_mutex_lock(&mutex);
-        if (head == nullptr) {
+        if (head == nullptr)
+        {
             std::cerr << "List is empty" << std::endl;
             pthread_mutex_unlock(&mutex);
             return NULL;
